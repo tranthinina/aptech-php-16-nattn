b3a0770b01633d7fa9fa9d0189db6eef32e80c0a
tranthinina <natran34k1.1@gmail.com>
2019-03-07 22:12:15 +0700

    MT Jewelry

---
+++ do-an-php/MTJEWELRY/resources/views/layouts/app.blade.php
@@ -0,0 +1,379 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+
+<!-- Mirrored from demo.7uptheme.com/html/bw-store/home-jewelry-02.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 27 Feb 2019 14:27:56 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1"/>
+	<meta name="description" content="BW Store is new Html theme that we have designed to help you transform your store into a beautiful online showroom. This is a fully responsive Html theme, with multiple versions for homepage and multiple templates for sub pages as well" />
+	<meta name="keywords" content="BW Store,7uptheme" />
+	<meta name="robots" content="noodp,index,follow" />
+	<meta name='revisit-after' content='1 days' />
+	<title>MT Jewelry | Home Jewelry</title>
+	<link href="../../../fonts.googleapis.com/css4792.css?family=Open+Sans:300,400,700%7cPlayfair+Display:400,700,400i,700i" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/libs/font-awesome.min.css"/>
+	<link rel="stylesheet" type="text/css" href="css/libs/ionicons.min.css"/>
+	<link rel="stylesheet" type="text/css" href="css/libs/bootstrap.min.css"/>
+	<link rel="stylesheet" type="text/css" href="css/libs/bootstrap-theme.min.css"/>
+	<link rel="stylesheet" type="text/css" href="css/libs/jquery.fancybox.min.css"/>
+	<link rel="stylesheet" type="text/css" href="css/libs/jquery-ui.min.css"/>
+	<link rel="stylesheet" type="text/css" href="css/libs/owl.carousel.min.css"/>
+	<link rel="stylesheet" type="text/css" href="css/libs/owl.transitions.min.css"/>
+	<link rel="stylesheet" type="text/css" href="css/libs/jquery.mCustomScrollbar.min.css"/>
+	<link rel="stylesheet" type="text/css" href="css/libs/owl.theme.min.css"/>
+	<link rel="stylesheet" type="text/css" href="css/libs/animate.min.css"/>
+	<link rel="stylesheet" type="text/css" href="css/libs/hover.min.css"/>
+	<link rel="stylesheet" type="text/css" href="css/theme.css" media="all"/>
+	<link rel="stylesheet" type="text/css" href="css/responsive.css" media="all"/>
+	<link rel="stylesheet" type="text/css" href="css/browser.css" media="all"/>
+	<!-- <link rel="stylesheet" type="text/css" href="css/rtl.css" media="all"/> -->
+</head>
+<body class="preload">
+<div class="wrap">
+	<header id="header">
+		<div class="top-header bg-dark">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6 col-sm-6 col-xs-12">
+						<div class="hot-news pull-left">
+							<h2 class="title14 text-uppercase white inline-block font-bold">News</h2>
+							<div class="hot-news-slider inline-block">
+								<div class="wrap-item" data-autoplay="true" data-itemscustom="[[0,1]]" data-pagination="false" data-navigation="true">
+									<div class="item">
+										<h3 class="title14"><a href="#" class="white">Lorem ipsum dolor consectetu</a></h3>
+									</div>
+									<div class="item">
+										<h3 class="title14"><a href="#" class="white">Lorem ipsum dolor consectetu</a></h3>
+									</div>
+									<div class="item">
+										<h3 class="title14"><a href="#" class="white">Lorem ipsum dolor consectetu</a></h3>
+									</div>
+									<div class="item">
+										<h3 class="title14"><a href="#" class="white">Lorem ipsum dolor consectetu</a></h3>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6 col-sm-6 col-xs-12">
+						<ul class="top-total-info list-inline-block pull-right">
+							<li>
+								<p class="desc white call-phone title12"><i class="fa fa-volume-control-phone"></i><span class="title14">Call: 01678 311 160</span></p>
+							</li>
+							<li>
+								<div class="top-social-network">
+									<a href="#" class="title12 white inline-block round"><i class="fa fa-facebook"></i></a>
+									<a href="#" class="title12 white inline-block round"><i class="fa fa-twitter"></i></a>
+									<a href="#" class="title12 white inline-block round"><i class="fa fa-pinterest-p"></i></a>
+								</div>
+							</li>
+							<li>
+								<a href="#" class="title12 white link-account"><i class="fa fa-user"></i><span class="title14">My Account</span></a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- End Top Header -->
+		<div class="header2 bg-white">
+			<div class="main-header2">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-3 col-sm-12 col-xs-12">
+							<div class="logo logo1 pull-left">
+								<a href="index-2.html">
+									<span class="logo-icon title24 round white bg-dark"><i class="fa fa-diamond"></i></span>
+									<strong class="play-font font-italic title30 dark text-uppercase">MT-</strong>
+									<span class="play-font font-italic font-normal title30 dark">Jewelry</span>
+								</a>
+							</div>
+							<!-- End logo -->
+						</div>
+						<div class="col-md-3 col-sm-4 col-xs-12">
+							<p class="desc free-order pull-right dark">Free delivery order over <strong>$100</strong></p>
+						</div>
+						<div class="col-md-6 col-sm-8 col-xs-12">
+							<form method="get" class="search-form form-round">
+								<div class="dropdown-box">
+									<a href="javascript:void(0)" class="dropdown-link">All Categories</a>
+									<ul class="list-none dropdown-list">
+										<li><a href="#">Ring</a></li>
+										<li><a href="#">Earring</a></li>
+										<li><a href="#">Necklace</a></li>
+										<li><a href="#">Bracelet</a></li>
+										<li><a href="#">Pentdant</a></li>
+									</ul>
+								</div>
+								<input type="text" name="s" placeholder="Search for product...">
+								<div class="submit-form">
+									<input type="submit" value="">
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- End Main Nav -->
+			<div class="header-nav2">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-9 col-sm-8 col-xs-12">
+							<nav class="main-nav main-nav1 pull-left">
+								<ul>
+									<li class="menu-item-has-children">
+										<a href="#">PRODUCT</a>
+										<ul class="sub-menu">
+                                                <li><a href="about.html">RING</a></li>
+                                                <li><a href="contact.html">EARRING</a></li>
+                                                <li><a href="about.html">NECKLACE</a></li>
+                                            <li><a href="contact.html">BRACELET</a></li>
+                                            <li><a href="contact.html">PENDANT</a></li>
+										</ul>
+                                    </li>
+                                    <li><a href="about.html">ABOUT US</a></li>
+                                    <li><a href="contact.html">CONTACT US</a></li>
+
+									
+									<li class="menu-item-has-children">
+										<a href="blog.html">Blog</a>
+										<ul class="sub-menu">
+											<li><a href="blog-list.html">Blog List</a></li>
+											<li><a href="blog.html">Blog List Large</a></li>
+											<li><a href="blog-grid.html">Blog Grid</a></li>
+											<li><a href="single.html">Blog Single</a></li>
+										</ul>
+									</li>
+								</ul>
+								<a href="#" class="toggle-mobile-menu"><span></span></a>
+							</nav>
+							<!-- End Main Nav -->
+						</div>
+						<div class="col-md-3 col-sm-4 col-xs-12">
+							<ul class="wrap-cart-top2 list-inline-block pull-right">
+								<li>
+									<a href="#" class="title18 compare-link" title="Compare"><span class="dark"><i class="icon ion-ios-loop-strong"></i></span></a>
+								</li>
+								<li>
+									<a href="#" class="title18 wishlist-link" title="Wishlist"><span class="dark"><i class="icon ion-android-favorite-outline"></i></span><sup class="title10 round white bg-dark">0</sup></a>
+								</li>
+								<li>
+									<div class="mini-cart-box mini-cart1 aside-box">
+										<a class="mini-cart-link" href="cart.html" title="Cart">
+											<span class="mini-cart-icon title18 dark"><i class="icon ion-bag"></i></span>
+											<span class="mini-cart-text">
+												<span class="mini-cart-number">4</span>
+												<span class="mini-cart-total-price hidden">$80.00</span>
+											</span>
+										</a>
+										<div class="mini-cart-content text-left">
+											<h2 class="title18 font-bold">(4) ITEMS IN MY CART</h2>
+											<div class="list-mini-cart-item">
+												<div class="product-mini-cart table-custom">
+													<div class="product-thumb">
+														<a href="detail.html" class="product-thumb-link"><img alt="" src="images/photos/glasses/dl-store-glasse-01.jpg"></a>
+													</div>
+													<div class="product-info">
+														<h3 class="title14 product-title play-font"><a href="#">D&L SO REAL POP SUNGLASSES pink</a></h3>
+														<div class="mini-cart-qty">
+															<span>1 x $40.00</span>
+														</div>
+													</div>
+													<div class="product-delete text-right">
+														<a href="#" class="remove-product"><i class="fa fa-trash"></i></a>
+													</div>
+												</div>
+												<div class="product-mini-cart table-custom">
+													<div class="product-thumb">
+														<a href="detail.html" class="product-thumb-link"><img alt="" src="images/photos/glasses/dl-store-glasse-03.jpg"></a>
+													</div>
+													<div class="product-info">
+														<h3 class="title14 product-title play-font"><a href="#">D&L SO REAL POP SUNGLASSES Yellow</a></h3>
+														<div class="mini-cart-qty">
+															<span>1 x $40.00</span>
+														</div>
+													</div>
+													<div class="product-delete text-right">
+														<a href="#" class="remove-product"><i class="fa fa-trash"></i></a>
+													</div>
+												</div>
+												<div class="product-mini-cart table-custom">
+													<div class="product-thumb">
+														<a href="detail.html" class="product-thumb-link"><img alt="" src="images/photos/glasses/dl-store-glasse-10.jpg"></a>
+													</div>
+													<div class="product-info">
+														<h3 class="title14 product-title play-font"><a href="#">D&L SO REAL POP SUNGLASSES black</a></h3>
+														<div class="mini-cart-qty">
+															<span>1 x $40.00</span>
+														</div>
+													</div>
+													<div class="product-delete text-right">
+														<a href="#" class="remove-product"><i class="fa fa-trash"></i></a>
+													</div>
+												</div>
+												<div class="product-mini-cart table-custom">
+													<div class="product-thumb">
+														<a href="detail.html" class="product-thumb-link"><img alt="" src="images/photos/glasses/dl-store-glasse-02.jpg"></a>
+													</div>
+													<div class="product-info">
+														<h3 class="title14 product-title play-font"><a href="#">D&L so real pop sunglass blue</a></h3>
+														<div class="mini-cart-qty">
+															<span>1 x $40.00</span>
+														</div>
+													</div>
+													<div class="product-delete text-right">
+														<a href="#" class="remove-product"><i class="fa fa-trash"></i></a>
+													</div>
+												</div>
+											</div>
+											<div class="mini-cart-total text-uppercase title18 font-bold clearfix">
+												<span class="pull-left">TOTAL</span>
+												<strong class="pull-right color play-font mini-cart-total-price">$160.00</strong>
+											</div>
+											<div class="mini-cart-button">
+												<a class="mini-cart-view shop-button" href="#">View cart </a>
+												<a class="mini-cart-checkout shop-button" href="#">Checkout</a>
+											</div>
+										</div>
+									</div>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- End Header Nav -->
+		</div>
+	</header>
+	<!-- End Header -->
+    
+    @yield('content')
+    
+	<!-- End Content -->
+	<footer id="footer">
+		<div class="container">
+			<div class="list-service-footer">
+				<ul class="list-none">
+					<li><a href="#"><img src="images/home/jewelry2/icon1.png" alt="" /></a> <span class="opaci">FREE DELIVERY FROM $99</span></li>
+					<li><a href="#"><img src="images/home/jewelry2/icon2.png" alt="" /></a> <span class="opaci">SECURE PAYMENT</span></li>
+					<li><a href="#"><img src="images/home/jewelry2/icon3.png" alt="" /></a> <span class="opaci">100% GUARANTEED</span></li>
+					<li><a href="#"><img src="images/home/jewelry2/icon4.png" alt="" /></a> <span class="opaci">MONEY BACK GUARANTEED</span></li>
+				</ul>
+			</div>
+		</div>
+		<div class="footer2">
+			<div class="container">
+				
+				<div class="main-footer2">
+					<div class="row">
+						<div class="col-md-4 col-sm-4 col-xs-12">
+							<div class="block-footer2">
+								<h2 class="title30 dark play-font font-italic">Newsletter</h2>
+								<p class="desc dark opaci">Sign up to our newsletter to receive updates on the art of Coin.</p>
+								<form class="form-newsletter2">
+									<input type="text" placeholder="EMAIL ADDRESS">
+									<input type="submit" value="Subscription">
+								</form>
+							</div>
+						</div>
+						<div class="col-md-4 col-sm-4 col-xs-12">
+							<div class="block-footer2">
+								<div class="logo logo-footer2 text-center">
+									<a href="index-2.html">
+										<span class="logo-icon title24 round white bg-dark"><i class="fa fa-diamond"></i></span>
+										<strong class="play-font  title30 dark text-uppercase">BW-</strong>
+										<span class="play-font font-normal title30 dark">Store</span>
+									</a>
+								</div>
+								<h2 class="title18 play-font dark text-center font-italic">Follow Us On</h2>
+								<div class="social-network-footer text-center">
+									<a href="#" class="inline-block round"><i class="fa fa-facebook"></i></a>
+									<a href="#" class="inline-block round"><i class="fa fa-twitter"></i></a>
+									<a href="#" class="inline-block round"><i class="fa fa-pinterest-p"></i></a>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4 col-sm-4 col-xs-12">
+							<div class="block-footer2">
+								<h2 class="title30 dark play-font font-italic">Concierge</h2>
+								<p class="desc dark opaci">Don't hesitate in contacting us for any questions you might have.</p>
+								<ul class="list-none contact-foter2">
+									<li>
+										<i class="fa fa-location-arrow dark"></i>
+										<span class="text-uppercase dark opaci">SHIPPING & RETURNS</span>
+									</li>
+									<li>
+										<i class="fa fa-question dark"></i>
+										<span class="text-uppercase dark opaci">FREQUENTLY ASKED QUESTIONS</span>
+									</li>
+									<li>
+										<i class="fa fa-volume-control-phone dark"></i>
+										<span class="text-uppercase dark opaci">+84 1678 311 160</span>
+									</li>
+									<li>
+										<i class="fa fa-envelope dark"></i>
+										<a class="dark opaci" href="mailto:contact.7uptheme@gmail.com">contact.7uptheme@gmail.com</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- End Main Footer -->
+				<div class="footer-bottom2">
+					<ul class="footer-menu text-uppercase text-center list-inline-block">
+						<li><a href="#" class="dark wobble-top">cONTACT US</a></li>
+						<li><a href="#" class="dark wobble-top">SITEMAP</a></li>
+						<li><a href="#" class="dark wobble-top">CAREERS</a></li>
+						<li><a href="#" class="dark wobble-top">REPRINTS &amp; PERMISSIONS</a></li>
+						<li><a href="#" class="dark wobble-top">Privacy &amp; Cookies</a></li>
+						<li><a href="#" class="dark wobble-top">Terms &amp; Conditions</a></li>
+						<li><a href="#" class="dark wobble-top">About Us</a></li>
+					</ul>
+					<p class="desc copyright-footer text-center"><span class="dark opaci"> Bwstore © 2018 All Rights Reserved. Design by:</span> <a href="http://7uptheme.com/" class="dark">7uptheme.com</a></p>
+				</div>
+			</div>
+		</div>
+	</footer>
+	<!-- End Footer -->
+	<div class="wishlist-mask">
+		<div class="wishlist-popup">
+			<span class="popup-icon"><i class="fa fa-bullhorn" aria-hidden="true"></i></span>
+			<p class="wishlist-alert">"BW Store Product" was added to wishlist</p>
+			<div class="wishlist-button">
+				<a href="#">Continue Shopping (<span class="wishlist-countdown">5</span>)</a>
+				<a href="#">Go To Shopping Cart</a>
+			</div>
+		</div>
+	</div>
+	<!-- End Wishlist Mask -->
+	<div id="loading">
+		<div id="loading-center">
+			<div id="loading-center-absolute">
+				<div class="object" id="object_four"></div>
+				<div class="object" id="object_three"></div>
+				<div class="object" id="object_two"></div>
+				<div class="object" id="object_one"></div>
+			</div>
+		</div>
+	</div>
+	<!-- End Preload -->
+	<a href="#" class="scroll-top dark"><i class="fa fa-angle-up"></i></a>
+</div>
+<script src="js/libs/jquery-3.2.1.min.js"></script>
+<script src="js/libs/bootstrap.min.js"></script>
+<script src="js/libs/jquery.fancybox.min.js"></script>
+<script src="js/libs/jquery-ui.min.js"></script>
+<script src="js/libs/owl.carousel.min.js"></script>
+<script src="js/libs/jquery.jcarousellite.min.js"></script>
+<script src="js/libs/jquery.mCustomScrollbar.min.js"></script>
+<script src="js/libs/jquery.elevatezoom.min.js"></script>
+<script src="js/libs/popup.min.js"></script>
+<script src="js/libs/timecircles.min.js"></script>
+<script src="js/libs/wow.min.js"></script>
+<script src="js/theme.js"></script>
+</body>
+
+<!-- Mirrored from demo.7uptheme.com/html/bw-store/home-jewelry-02.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 27 Feb 2019 14:27:56 GMT -->
+</html>
---
+++ do-an-php/README.md
@@ -0,0 +1,2 @@
+# do-an-php
+MINH TAM JEWELRY
